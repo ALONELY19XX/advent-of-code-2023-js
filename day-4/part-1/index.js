eed7445ad481fb58aeb63f75f7bfd228474660fc
ALONELY19XX <valentin.wagner1998@gmail.com>
2023-12-06 21:28:29 +0100

    Removed console log and fixed switched up files

--- day-4/part-1/index.js
+++ day-4/part-1/index.js
@@ -7,20 +7,13 @@ import { once } from 'node:events'
 // Dirname under usage of ES6 modules in node
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+// Buffer to hold points for each card
+const points = []
+
 // Readline interface, so we can read the input file line-by-line instead of all-at-once
 const rl = createInterface({
    input: createReadStream(join(__dirname, 'input.txt')),
 })
-
-// Each Array index holds the amount of cards for the given card number
-// Since arrays are zero-indexed, the representation is established:
-// Index 0 -> holds total amount of Card 1
-// Index 1 -> holds total amount of Card 2
-// ...
-// Index N -> holds total amount of Card N+1
-const indexedCardSets = []
-
-let cardNo = 0 // Current card number (zero-indexed; here 0 refers to Card 1)
 
 // Once new line is read
 rl.on('line', (line) => {
@@ -50,52 +43,20 @@ rl.on('line', (line) => {
       }
    })
 
-   // If its the first card (which only exists once we set it to 1)
-   if (cardNo === 0) {
-      indexedCardSets.push(1)
+   // Formula for more than one match: 2^(totalMatches-1)
+   if (totalMatchingNumbers > 1) {
+      points.push(Math.pow(2, totalMatchingNumbers - 1))
    }
-   // Otherwise
+   // Otherwise push 0 or 1 (no further logic required for these cases)
    else {
-      // If no previous card match created copies for the current card
-      // (meaning the array at the index for this card holds no value)
-      // we have to initialize it by setting it to 1
-      // info: it will stay at 1, since no card following this one can increase previous cards
-      if (indexedCardSets[cardNo] === undefined) {
-         indexedCardSets[cardNo] = 1
-      }
-      // If previous card matches created copies for the current card before
-      // we have to increase the counter for this card by one (to include the original card)
-      else {
-         indexedCardSets[cardNo]++
-      }
+      points.push(totalMatchingNumbers)
    }
-
-   // For each match modify the range (= total matches) of subsequent cards
-   for (let i = 1; i <= totalMatchingNumbers; ++i) {
-      // If subsequent cards is not tracking any copy counter yet, initialize by
-      // amount of current card, since each instance of the current card (original+copies)
-      // will create a copy for the subsequent card
-      if (indexedCardSets[cardNo + i] === undefined) {
-         indexedCardSets[cardNo + i] = indexedCardSets[cardNo]
-      }
-      // If subsequent cards is already tracking copy counter, modify by
-      // amount of current card, since each instance of the current card (original+copies)
-      // will create a copy for the subsequent card
-      else {
-         indexedCardSets[cardNo + i] += indexedCardSets[cardNo]
-      }
-   }
-
-   // increase card number as we are about to move to next line of input file -> next card
-   cardNo++
 })
 
 // Wait until all lines were processed
 await once(rl, 'close')
 
-console.log(indexedCardSets)
-
 // Get result
-const result = indexedCardSets.reduce((acc, num) => (acc += num), 0)
+const result = points.reduce((acc, num) => (acc += num), 0)
 
 console.log('The result is:', result)
